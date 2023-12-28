ebc494e6fb5d7673a82dcf990d1b7f671cd52682
Daniel Rey <reyvillanuevadaniel@gmail.com>
2023-12-28 16:09:25 +0800

    hello world

--- components/server/overview/progress-filtered-data.tsx
+++ components/server/overview/progress-filtered-data.tsx
@@ -9,6 +9,8 @@ import {
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 import { FilterProgress } from "@/constants/types";
+import { cn } from "@/lib/utils";
+import { CSVLink } from "react-csv";
 
 interface ProgressFilteredDataProps {
   championName: string;
@@ -29,6 +31,36 @@ export default function ProgressFilteredData({
   setIsSuccess,
   isSuccess,
 }: ProgressFilteredDataProps) {
+  function getColor() {
+    if (data.resolution_rate >= 50) {
+      return "text-green-500";
+    }
+    if (data.resolution_rate >= 30 && data.resolution_rate <= 49) {
+      return "text-orange-500";
+    }
+
+    return "text-red-500";
+  }
+
+  function getDownloadableData() {
+    return [
+      [
+        "Ticket Count",
+        "Closed Ticket Count",
+        "Open Ticket Count",
+        "Cancelled Ticket Count",
+        "Resolution Rate",
+      ],
+      [
+        data.ticket_count,
+        data.closed_ticket_count,
+        data.open_ticket_count,
+        data.cancelled_ticket_count,
+        data.resolution_rate,
+      ],
+    ];
+  }
+
   return (
     <AlertDialog
       onOpenChange={(open) => {
@@ -51,29 +83,46 @@ export default function ProgressFilteredData({
         <div>
           <div className="flex items-center justify-between w-full text-lg">
             <p>No of Tickets</p>
-            <h4 className="text-xl mr-4">{data.ticket_count}</h4>
+            <h4 className="text-2xl mr-4">{data.ticket_count}</h4>
           </div>
-          <div className="flex items-center justify-between w-full text-lg">
+          <div
+            className={cn(
+              "flex items-center justify-between w-full text-lg",
+              getColor()
+            )}
+          >
             <p>Closed</p>
-            <h4 className="text-xl mr-4">{data.closed_ticket_count}</h4>
+            <h4 className="text-2xl mr-4">{data.closed_ticket_count}</h4>
           </div>
           <div className="flex items-center justify-between w-full text-lg">
             <p>Open</p>
-            <h4 className="text-xl mr-4">{data.open_ticket_count}</h4>
+            <h4 className="text-2xl mr-4">{data.open_ticket_count}</h4>
           </div>
           <div className="flex items-center justify-between w-full text-lg">
             <p>Cancelled</p>
-            <h4 className="text-xl mr-4">{data.cancelled_ticket_count}</h4>
+            <h4 className="text-2xl mr-4">{data.cancelled_ticket_count}</h4>
           </div>
           <hr />
-          <div className="flex items-center justify-between w-full text-lg mt-3">
+          <div
+            className={cn(
+              "flex items-center justify-between w-full text-lg mt-3",
+              getColor()
+            )}
+          >
             <p>Resolution Rate</p>
-            <h4 className="text-4xl mr-4">{data.resolution_rate}%</h4>
+            <h4 className="text-4xl mr-2">{data.resolution_rate}%</h4>
           </div>
         </div>
-        <AlertDialogFooter>
+        <AlertDialogFooter className="mt-4">
           <AlertDialogCancel>Okay</AlertDialogCancel>
-          <AlertDialogAction>Download CSV</AlertDialogAction>
+          <AlertDialogAction asChild>
+            <CSVLink
+              data={getDownloadableData()}
+              filename={`${championName}-${start}-${end}`}
+            >
+              Download CSV
+            </CSVLink>
+          </AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
     </AlertDialog>
