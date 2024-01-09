61934c50503e5504600c862eb500ab8d57238d2f
Daniel Rey <reyvillanuevadaniel@gmail.com>
2024-01-09 17:22:32 +0800

    fix the ticket types tab

--- app/department/it/ticket-types/page.tsx
+++ app/department/it/ticket-types/page.tsx
@@ -1,5 +1,7 @@
 import { columns } from "@/components/client/ticket-types/columns";
 import { DataTable } from "@/components/client/ticket-types/data-table";
+import TabMutator from "@/components/helper/tab-mutator";
+import { AvailableTabs } from "@/constants/enums";
 import { TicketTypeColumns } from "@/constants/types";
 
 async function getData(): Promise<TicketTypeColumns[]> {
@@ -108,6 +110,10 @@ export default async function TicketTypesPage() {
 
   return (
     <div>
+      <TabMutator
+        availableTab={AvailableTabs["Ticket Types"]}
+        key={"TicketTypesPage"}
+      />
       <h1 className="w-full text-start text-lg">Ticket Type</h1>
       <DataTable columns={columns} data={data} />
     </div>
