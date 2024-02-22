00999fbb41ba9f8bf0f5eb75640b698ec7b88465
Daniel Rey <danielrey@devexsolutions.com>
2024-02-21 21:02:12 -0800

    added something interesting

--- components/client/feed/display-buttons.tsx
+++ components/client/feed/display-buttons.tsx
@@ -7,6 +7,8 @@ import { useRouter } from "next/navigation";
 import { IoClose, IoFolderOpenOutline } from "react-icons/io5";
 import { TbMessageCancel } from "react-icons/tb";
 import ConfirmationBox from "./ConfirmationBox";
+import { useState } from "react";
+import { LoadingButton } from "@/components/utils/LoadingButton";
 
 interface DispalyButtonsProps {
   status: string;
@@ -24,6 +26,8 @@ export default function DispalyButtons({
   const router = useRouter();
   const role = Cookies.get("it_access_level")?.toLowerCase();
 
+  const [loading, setLoading] = useState(false);
+
   function displayFeedback(message: string, success: boolean) {
     if (success) {
       toast({
@@ -38,29 +42,18 @@ export default function DispalyButtons({
     }
   }
 
-  async function handleOpenClick() {
-    const { message, success } = await updateTicketStatusAction("open", id);
+  async function handleButtonClick(toStatus: string) {
+    setLoading(true);
+    const { message, success } = await updateTicketStatusAction(toStatus, id);
     displayFeedback(message, success);
-  }
-
-  async function handleCloseClick() {
-    const { message, success } = await updateTicketStatusAction("closed", id);
-    displayFeedback(message, success);
+    
     if (success && role !== "requestor") {
       router.back();
     }
-  }
 
-  async function handleCancelClick() {
-    const { message, success } = await updateTicketStatusAction(
-      "cancelled",
-      id
-    );
-    displayFeedback(message, success);
-    if (success && role !== "requestor") {
-      router.back();
-    }
+    setLoading(false);
   }
+  
 
   function content() {
     if (status === "open") {
@@ -71,7 +64,7 @@ export default function DispalyButtons({
               buttonText="Cancel Ticket"
               Icon={TbMessageCancel}
               description="If you proceed, the ticket you created will be treated like it wont exists. Are you still going to cancel the ticket?"
-              handleButtonClick={handleCancelClick}
+              handleButtonClick={() => handleButtonClick("cancelled")}
               title="Are you sure you want to cancel the ticket?"
             />
           )}
@@ -80,7 +73,7 @@ export default function DispalyButtons({
               buttonText="Close Ticket"
               Icon={IoClose}
               description="If you proceed, the ticket you created will be be closed and no one else could able to comment to it. Are you still going to close the ticket?"
-              handleButtonClick={handleCloseClick}
+              handleButtonClick={() => handleButtonClick("closed")}
               title="Are you sure you want to close the ticket?"
             />
           )}
@@ -90,7 +83,7 @@ export default function DispalyButtons({
     return (
       <Button
         variant={"noVariant"}
-        onClick={handleOpenClick}
+        onClick={() => handleButtonClick("open")}
         className="text-center flex items-center justify-center gap-1 p-6 rounded-full hover:cursor-pointer bg-stone-900 text-stone-50 hover:bg-stone-900/90 dark:bg-stone-50 dark:text-stone-900 dark:hover:bg-stone-50/90"
       >
         <span className="text-xl">
@@ -103,7 +96,7 @@ export default function DispalyButtons({
 
   return (
     <>
-      <div className="w-full flex flex-col gap-2 py-3">{content()}</div>
+      <div className="w-full flex flex-col gap-2 py-3">{loading ? <LoadingButton isFullWidth={false} /> : content()}</div>
     </>
   );
 }
